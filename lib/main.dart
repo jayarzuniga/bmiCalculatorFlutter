e3c79433b27e901209db940bd7f711b2926ad881
Jay-r <jhayar.zuniga004@gmail.com>
2024-08-30 12:39:54 +0800

    initial commit for bmi calculator dart project

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget{
+  @override
+  Widget build (BuildContext context){
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: const Color(0xFF0A0E21),
+            appBarTheme: const AppBarTheme(
+              color: Color(0xFF0A0E21),
+              titleTextStyle: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+              ),
+            ),
+      ),
+      home: InputPage(),
+    );
+  }
+}
+
+
+
